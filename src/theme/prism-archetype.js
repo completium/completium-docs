dda225d5c79dc4662d5e063cc093916776ddcdf0
Benoît Rognier <benoit.rognier@edulera.com>
2020-10-10 13:27:03 +0200

    fix archetype theme

--- src/theme/prism-archetype.js
+++ src/theme/prism-archetype.js
@@ -14,7 +14,7 @@
 		],
 		'number': /\b(?:0x[\da-f][\da-f_]+|(?:0[bo])?\d[\d_]*\.?[\d_]*(?:e[+-]?[\d_]+)?)/i,
 		'directive': {
-			pattern: /\B#\w+/,
+			pattern: /entry\s+\w+/,
 			alias: 'important'
 		},
 		'label': {
@@ -22,23 +22,23 @@
 			alias: 'function'
 		},
 		'type_variable': {
-			pattern: /\B'\w+/,
+			pattern: /\w+\s+:\s+\w+/,
 			alias: 'function'
 		},
-		'variant': {
-			pattern: /`\w+/,
+		'variable': {
+			pattern: /var \w+/,
 			alias: 'variable'
 		},
 		'module': {
-			pattern: /\b[A-Z]\w+/,
+			pattern: /entry\s+\w+/,
 			alias: 'variable'
 		},
 		// For the list of keywords and operators,
 		// see: http://caml.inria.fr/pub/docs/manual-ocaml/lex.html#sec84
-		'keyword': /\b(?:as|asset|require|effect|entry|assert|begin|end|do|done|else|exception|external|for|function|functor|if|in|include|inherit|initializer|lazy|let|match|method|module|mutable|new|nonrec|object|of|open|private|rec|sig|struct|then|to|try|type|val|value|virtual|when|where|while|with)\b/,
+		'keyword': /\b(?:as|asset|require|effect|identified by|initialized by|entry|assert|begin|end|do|done|else|exception|external|for|if|in|include|inherit|initializer|lazy|let|match|method|module|mutable|new|nonrec|object|of|open|private|rec|sig|struct|then|to|try|type|val|virtual|when|where|while|with)\b/,
 		'boolean': /\b(?:false|true)\b/,
 		// Custom operators are allowed
-		'operator': /:=|[=<>@^|&+\-*\/$%!?~][!$%&*+\-.\/:<=>?@^|~]*|\b(?:and|asr|land|lor|lsl|lsr|lxor|mod|or|update|addupdate)\b/,
+		'operator': /:=|\+=|\-=|[=<>@^|&+\-*\/$%!?~][!$%&*+\-.\/:<=>?@^|~]*|\b(?:and|asr|land|lor|lsl|lsr|lxor|mod|or|update|addupdate)\b/,
 		'punctuation': /[(){}\[\]|.,:;]|\b_\b/
 	};
 }(Prism));
