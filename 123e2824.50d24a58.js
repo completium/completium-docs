ca43768f2472e1fc60d85561d25f0bcf5a2b2e49
Benoît Rognier <benoit.rognier@edulera.com>
2020-10-10 12:57:19 +0200

    Deploy website - based on a78e1482c4aca97bcc385a42e245860199d6d1a7

---
+++ 123e2824.50d24a58.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{60:function(s){s.exports=JSON.parse('{"allTagsPath":"/completium/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/completium/blog/tags/docusaurus"}')}}]);
